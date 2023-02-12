ffdbd76b03de5319592a0870886b7c7d657d2def
thibautvangoethem <thibautvangoethem2@gmail.com>
2023-02-12 21:50:42 +0100

    start with the random object spawner

--- main.gd
+++ main.gd
@@ -1,9 +1,19 @@
 extends Node2D
 
 @onready var characterNode=$Pinda
-# Called when the node enters the scene tree for the first time.
+@onready var RandomObjectSpawnHandler= load("res://randomObjectSpawnHandler.gd").new()
+#Use seperate timer to handle random objects, as we want this to be based om actual time and not any physics process/framerate
+#Counting the delta would also work, but I prefer this
+var RandomObjectSpawnTimer = null
+
+
 func _ready():
-	pass # Replace with function body.
+	RandomObjectSpawnTimer=Timer.new()
+	RandomObjectSpawnTimer.connect("timeout", triggerRandomObjectSpawnTick)
+	RandomObjectSpawnTimer.set_one_shot(false)
+	add_child(RandomObjectSpawnTimer)
+	RandomObjectSpawnTimer.start(1.0)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,3 +24,7 @@ func _on_ground_body_entered(body):
 	if(body==characterNode):
 		$Scrolling.stopGame()
 		$Pinda.stopGame()
+
+func triggerRandomObjectSpawnTick():
+	print("tick")
+	RandomObjectSpawnHandler.tick()
---
+++ randomObjectSpawnHandler.gd
@@ -0,0 +1,5 @@
+extends Node
+
+#TODO heel dit ding, geen idee of dit ook wel degelijk goed is
+func tick():
+	pass	
