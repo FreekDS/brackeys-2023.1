fe8917ab99318cc64b68480a29688dc1e9ba69d4
thibautvangoethem <thibautvangoethem2@gmail.com>
2023-02-12 17:48:48 +0100

    falling helicopter + placeholder textures

---
+++ main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var characterNode=$Pinda
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_ground_body_entered(body):
+	if(body==characterNode):
+		$Scrolling.stopGame()
