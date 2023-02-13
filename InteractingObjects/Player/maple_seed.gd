380f63c9e0901f406874dae159ea3c62bde8390b
Freek <freek.de.sagher21@gmail.com>
2023-02-13 17:29:03 +0100

    https://www.youtube.com/shorts/9LqgSBDiS5g

--- InteractingObjects/Player/maple_seed.gd
+++ InteractingObjects/Player/maple_seed.gd
@@ -1,39 +1,41 @@
 extends CharacterBody2D
 
-var enabled=true
+var enabled = true
 
-var marginx=25
-var marginy=10
+var marginx = 25
+var marginy = 10
+
+
 func _physics_process(delta):
-	if(enabled):
+	if enabled:
 			
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		
-		var pos=self.get_global_transform_with_canvas().origin
-		var mousePos=get_viewport().get_mouse_position()
+		var pos = self.get_global_transform_with_canvas().origin
+		var mousePos = get_viewport().get_mouse_position()
 		velocity = pos.direction_to(mousePos) * 300
 		
-		var diff=abs(pos-mousePos)
-		if(diff.x<10):
-			velocity.x=0	
-		if(diff.y<10):
-			velocity.y=0	
+		var diff = abs(pos - mousePos)
+		if diff.x < 10:
+			velocity.x=0
+		if diff.y < 10:
+			velocity.y=0
 			
-		var max=get_viewport().get_visible_rect().size
-		if (pos.x>(max.x-marginx)) and velocity.x>0:
-			velocity.x=0
-		if pos.x<marginx and velocity.x<0:
-			velocity.x=0
-		if (pos.y>(max.y-marginy)) and velocity.y>0:
-			velocity.y=0
-		if pos.y<marginy and velocity.y<0:
-			velocity.y=0
+		var maximum = get_viewport().get_visible_rect().size
+		if  pos.x > (maximum.x-marginx) and velocity.x > 0:
+			velocity.x = 0
+		if pos.x < marginx and velocity.x < 0:
+			velocity.x = 0
+		if pos.y > (maximum.y-marginy) and velocity.y > 0:
+			velocity.y = 0
+		if pos.y < marginy and velocity.y < 0:
+			velocity.y = 0
 		
 		move_and_slide()
 		
 		self.rotation_degrees+=delta*1000
 		
 func stopGame():
-	velocity=Vector2(0,0)
-	enabled=false
+	velocity = Vector2(0,0)
+	enabled = false
