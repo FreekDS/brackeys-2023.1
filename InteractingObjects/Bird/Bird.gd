0965ecb3c6174dfd5e1a689d2ac604e57f1dda89
Freek <freek.de.sagher21@gmail.com>
2023-02-13 21:12:56 +0100

    vogeltje

--- InteractingObjects/Bird/Bird.gd
+++ InteractingObjects/Bird/Bird.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @export var sourcePos : Marker2D 
 @export var targetPos : Marker2D
 
+@onready var sprite = $BirbPlaceholder
+
 var isFlying = false
 var direction: Vector2 = Vector2.ZERO
 
@@ -12,13 +14,18 @@ var direction: Vector2 = Vector2.ZERO
 func _ready():
 	assert(sourcePos != null, "loempen uil")
 	assert(targetPos != null, "loempen uil x2")
+	
+func start():
 	direction = (targetPos.position - sourcePos.position).normalized()
-
+	sprite.flip_h = direction.x > 0
+	isFlying = true
+#	$kahkaa.play()
 
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("ui_accept"): #just vr te testen
 		isFlying = true
 	if isFlying:
+		
 		velocity = direction * speed
 		move_and_slide()
 
---
+++ InteractingObjects/Bird/BirdSpawner.gd
@@ -0,0 +1,87 @@
+extends Node2D
+
+var screenOffset : Vector2
+
+@onready var src = $src as Marker2D
+@onready var target = $target as Marker2D
+@onready var bird = $Bird
+
+enum EDGE {
+	LEFT,
+	RIGHT,
+	TOP,
+	BOTTOM
+}
+
+var sourceEdge: EDGE
+var targetEdge: EDGE
+
+
+var sourcePos = Vector2.ZERO
+var targetPos = Vector2.ZERO
+
+
+func _ready():
+	screenOffset.x = ProjectSettings.get("display/window/size/viewport_width") / 2
+	screenOffset.y = ProjectSettings.get("display/window/size/viewport_height") / 2
+	
+	vliegVogeltjeVlieg()
+
+func vliegVogeltjeVlieg():
+	getFirstPoint()
+	getSecondPoint()
+	print(EDGE.keys()[sourceEdge], sourcePos)
+	print(EDGE.keys()[targetEdge], targetPos)
+	
+	src.position = sourcePos
+	target.position = targetPos
+	
+	bird.position = sourcePos
+	bird.start()
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		vliegVogeltjeVlieg()
+
+func sampleX() -> float:
+	return randf_range(-screenOffset.x, screenOffset.x)
+	
+func sampleY() -> float:
+	return randf_range(-screenOffset.y, screenOffset.y)
+
+func randEdge() -> EDGE:
+	return EDGE.values().pick_random()
+	
+func getFirstPoint():
+	sourceEdge = randEdge()
+	while sourceEdge == EDGE.BOTTOM:
+		sourceEdge = randEdge()
+	var target = Vector2.ZERO
+	sourcePos = pointAlongEdge(sourceEdge)
+	
+func getSecondPoint():
+	targetEdge = randEdge()
+	while sourceEdge == targetEdge:
+		targetEdge = randEdge()
+	targetPos = pointAlongEdge(targetEdge)
+
+
+func pointAlongEdge(edge: EDGE) -> Vector2:
+	var pos := Vector2.ZERO
+	match edge:
+		EDGE.LEFT:
+			pos.x =  -screenOffset.x
+			pos.y = sampleY()
+			pass
+		EDGE.RIGHT:
+			pos.x = screenOffset.x
+			pos.y = sampleY()
+			pass
+		EDGE.BOTTOM:
+			pos.y = screenOffset.y
+			pos.x = sampleX()
+		EDGE.TOP:
+			pos.y = -screenOffset.y
+			pos.x = sampleX()
+	return pos
+
