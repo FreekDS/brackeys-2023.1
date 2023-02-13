9507027b0817ed04d8b7014844e08951ed3c5483
thibautvangoethem <thibautvangoethem2@gmail.com>
2023-02-13 21:02:24 +0100

    gameplay loop

--- Common/main.gd
+++ Common/main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var characterNode=$MapleSeed
-@onready var dedlmaoScreen = $CanvasLayer/DeadScreen
+@onready var dedlmaoScreen= $DeasScreenPapa/DeadScreen
 @onready var RandomObjectSpawnHandler= load("res://randomObjectSpawnHandler.gd").new()
 #Use seperate timer to handle random objects, as we want this to be based om actual time and not any physics process/framerate
 #Counting the delta would also work, but I prefer this
@@ -19,6 +19,7 @@ func _on_ground_body_entered(body):
 	if body == characterNode:
 		$Scrolling.stopGame()
 		characterNode.stopGame()
+		dedlmaoScreen.visible=true
 		dedlmaoScreen.play()
 		
 func triggerRandomObjectSpawnTick():
--- UI/dead_screen.gd
+++ UI/dead_screen.gd
@@ -1,6 +1,9 @@
 extends Control
 
-
 func play():
 	visible = true
 	$AudioStreamPlayer.play()
+
+
+func _on_button_pressed():
+	get_tree().reload_current_scene()
