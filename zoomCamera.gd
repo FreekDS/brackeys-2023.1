ec330a5c6e1fb2f736144be6b66c68727589ddab
thibautvangoethem <thibautvangoethem2@gmail.com>
2023-02-15 21:07:50 +0100

    better zoom on start

--- zoomCamera.gd
+++ zoomCamera.gd
@@ -5,6 +5,6 @@ extends Camera2D
 func zoomToSeed(location:Vector2):
 	var zoomTween=create_tween()
 	zoomTween.set_parallel(true)
-	zoomTween.tween_property(self,"zoom",self.zoom + Vector2(1, 1),3.0)
+	zoomTween.tween_property(self,"zoom",self.zoom + Vector2(1.75, 1.75),3.0)
 	zoomTween.tween_property(self,"position",location,3.0)
 	
