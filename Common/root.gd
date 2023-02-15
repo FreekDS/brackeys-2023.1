7dbf400ae04cf2345acf3f56aea609eef1647919
thibautvangoethem <thibautvangoethem2@gmail.com>
2023-02-15 20:54:16 +0100

    start screen

--- Common/root.gd
+++ Common/root.gd
@@ -1,16 +1,22 @@
 extends Node2D
 var difficulty =0
+#round and difficulty split to have the aging of the lumberjack be different currently not used
+var round =0
 var gameplay=preload("res://Gameplay.tscn")
 var currentMain=null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	$Toremove/Seed.pause()
+	
+func startGame():
 	currentMain=gameplay.instantiate()
 	add_child(currentMain)
 	currentMain.connect("win",_on_main_win)
-
 func _on_main_win():
 	difficulty+=1
+	round+=1
 	remove_child(currentMain)
 	currentMain=gameplay.instantiate()
 	add_child(currentMain)
@@ -18,3 +24,16 @@ func _on_main_win():
 	currentMain.difficulty=difficulty
 	
 	
+func removeAndStart():
+	remove_child($Toremove)
+	$Menu.visible=false
+	startGame()
+	
+func _on_button_pressed():	
+	$Toremove/ZoomCamera.zoomToSeed($Toremove/Seed.position)
+	var timer=Timer.new()
+	timer.connect("timeout", removeAndStart)
+	timer.set_one_shot(true)
+	add_child(timer)
+	timer.start(3)
+
---
+++ zoomCamera.gd
@@ -0,0 +1,10 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func zoomToSeed(location:Vector2):
+	var zoomTween=create_tween()
+	zoomTween.set_parallel(true)
+	zoomTween.tween_property(self,"zoom",self.zoom + Vector2(1, 1),3.0)
+	zoomTween.tween_property(self,"position",location,3.0)
+	
