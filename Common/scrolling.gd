3df3f5c67e93d021fe055e28f42d0a4c0b80c2fc
Freek <freek.de.sagher21@gmail.com>
2023-02-17 18:02:15 +0100

    ik weet ni wa ik gechanged heb maja

--- Common/scrolling.gd
+++ Common/scrolling.gd
@@ -9,7 +9,12 @@ var active = true
 func _physics_process(delta):
 	if active:
 		self.position -= Vector2(1.5*delta*speed,delta*speed/1.5)
-		
+
+
+func scrollAlongDistance(distanceXY):
+	
+	pass
+
 
 func stopGame():
 	active=false
