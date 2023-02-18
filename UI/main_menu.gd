7d9a33747572381b3deb0cb91c025701de2f0a09
Freek <freek.de.sagher21@gmail.com>
2023-02-18 15:17:05 +0100

    mnenue

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _ready():
-	update_highscore(100)
+	update_highscore(0)
 
 func _on_play_button_pressed():
 	print("PLAY")
@@ -16,4 +16,8 @@ func _on_credits_button_pressed():
 	print("Credits")
 	
 func update_highscore(score=0):
+	if score == 0:
+		$Highscore.visible = false
+		return
+	$Highscore.visible = true
 	$Highscore.set_text("Highscore: " + str(score) + " generations")
